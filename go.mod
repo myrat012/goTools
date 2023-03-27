06eeb4f89c38f1d5164d1176231ab184eb814ebc
Agamyrat <agamyrat.e@millione.tm>
2023-03-27 23:21:07 +0500

    change func name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/myrat012/goTools
+module github.com/myrat012/goTools/v2
 
 go 1.20
--- goTools.go
+++ goTools.go
@@ -1,6 +1,6 @@
 package goTools
 
-func Contains(a []string, x string) bool {
+func InSlice(a []string, x string) bool {
 	for _, n := range a {
 		if x == n {
 			return true
